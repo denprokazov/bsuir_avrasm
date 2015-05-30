9b8f5fd3e3127b692c8cc7a7dfc3f7e9ab3b041e
denprokazov <denprokazov@gmail.com>
2015-05-30 12:40:39 +0300

    lab4
    led blink with program delay and timer0 delay
    
    for atmega2560 on 4MHz clock

---
+++ lab4/AvrBuild.bat
@@ -0,0 +1,2 @@
+@ECHO OFF
+"C:\Program Files (x86)\Atmel\AVR Tools\AvrAssembler2\avrasm2.exe" -S "E:\BSUIR\3kurs\2sem\MPSU\labs\lab4\labels.tmp" -fI -W+ie -C V3 -o "E:\BSUIR\3kurs\2sem\MPSU\labs\lab4\lab4.hex" -d "E:\BSUIR\3kurs\2sem\MPSU\labs\lab4\lab4.obj" -e "E:\BSUIR\3kurs\2sem\MPSU\labs\lab4\lab4.eep" -m "E:\BSUIR\3kurs\2sem\MPSU\labs\lab4\lab4.map" "E:\BSUIR\3kurs\2sem\MPSU\labs\lab4\lab42.asm"
